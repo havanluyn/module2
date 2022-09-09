06f59c5b8fe96b4dd01d751da3ec6604264ef8b4
havanluyn <hvluyn@gmail.com>
2022-09-09 08:38:08 +0700

    thuchanh2

---
+++ thuchanh1.1/src/ThucHanh2.java
@@ -0,0 +1,16 @@
+public class ThucHanh2 {
+    public static void main(String[] args) {
+        int i = 10;
+        float a = 20.0f;
+        double b = 20.5;
+        boolean c = true;
+        char d = 'y';
+        String e = "Toi la ai";
+        System.out.println("i = " + i);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+    }
+}
