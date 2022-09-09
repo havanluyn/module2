22dc52856635ab80e37327e79adf389dcb64815b
havanluyn <hvluyn@gmail.com>
2022-09-09 08:19:07 +0700

    module2

---
+++ thuchanh1.1/src/Main.java
@@ -0,0 +1,10 @@
+import sun.util.calendar.BaseCalendar;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is "+ now);
+    }
+} 
