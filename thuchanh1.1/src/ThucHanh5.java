758e51a3052ac89e2aee2c3ee463bb95c55b78b4
havanluyn <hvluyn@gmail.com>
2022-09-09 09:22:28 +0700

    thuchanh5

---
+++ thuchanh1.1/src/ThucHanh5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ThucHanh5 {
+    public static void main(String[] args) {
+        int month;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Nhap thang muon kiem tra:");
+        month=sc.nextInt();
+        while(month<1||month>12){
+            System.out.println("Nhap thang khong phu hop,hay nhap lai:");
+            month=sc.nextInt();
+        }
+        switch (month){
+            case 2:
+                System.out.println("Thang 2 co 28 hoac 29 ngay");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("Thang "+month+" co 31 ngay");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("Thang "+month+" co 30 ngay");
+                break;
+        }
+
+    }
+}
