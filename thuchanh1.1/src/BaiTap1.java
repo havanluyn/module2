ffa226546c6157672b0b95d24952d2431f5e4c30
havanluyn <hvluyn@gmail.com>
2022-09-09 10:14:43 +0700

    baitap1

---
+++ thuchanh1.1/src/BaiTap1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BaiTap1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Nhap ho va ten:");
+        String Fullname=sc.nextLine();
+        System.out.println("Hello ! "+Fullname);
+    }
+}
