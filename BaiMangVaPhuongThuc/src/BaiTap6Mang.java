2f5325d363b5c4b672f00be2536e60ab1b5fd647
havanluyn <hvluyn@gmail.com>
2022-09-13 15:17:32 +0700

    aa

--- BaiMangVaPhuongThuc/src/BaiTap6Mang.java
+++ BaiMangVaPhuongThuc/src/BaiTap6Mang.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class BaiTap6Mang {
     public static void main(String[] args) {
         char ch;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Nhap chuoi muon kiem tra");
-        String str= sc.nextLine();
+        String str = sc.nextLine();
         System.out.println("Nhap ki tu muon kiem tra");
         ch = sc.next().charAt(0);
-        int count=0;
-        for (int i=0;i<str.length();i++){
-            if (str.charAt(i)== ch){
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
                 count++;
             }
         }
-        System.out.println("Co "+count+" ki tu "+ch+" trong chuoi");
+        System.out.println("Co " + count + " ki tu " + ch + " trong chuoi");
     }
 }
--- BaiMangVaPhuongThuc/src/ThucHanhMang3.java
+++ BaiMangVaPhuongThuc/src/ThucHanhMang3.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class ThucHanhMang3 {
     public static void main(String[] args) {
-        double doF,doC,count;
-        Scanner sc=new Scanner(System.in);
+        double doF, doC, count;
+        Scanner sc = new Scanner(System.in);
         System.out.println("--- 1. Chuyen do F sang do C----");
         System.out.println("--- 2. Chuyen do C sang do F----");
-        count=sc.nextInt();
-        while (count!=1&&count!=2){
+        count = sc.nextInt();
+        while (count != 1 && count != 2) {
             System.out.println("Ban chi duoc chon 1 hoac 2");
-            count=sc.nextDouble();
+            count = sc.nextDouble();
         }
-        if (count==1){
-            System.out.println("Moi ban nhap do F muon chuyen");
-            doF=sc.nextDouble();
-            doC=(5.0/9)*(doF-32);
-            System.out.printf(doF+" doF==>doC:%.4fC",doC);
+        if (count == 1) {
+            System.out.println("Moi ban nh ap do F muon chuyen");
+            doF = sc.nextDouble();
+            doC = (5.0 / 9) * (doF - 32);
+            System.out.printf(doF + " doF==>doC:%.4fC", doC);
         }
-        if (count==2){
+        if (count == 2) {
             System.out.println("Moi ban nhap do C muon chuyen");
-            doC=sc.nextDouble();
-            doF=doC/(5.0*9)+32;
-            System.out.printf(doC+" doC==>doF:%.4fC",doF);
+            doC = sc.nextDouble();
+            doF = doC / (5.0 * 9) + 32;
+            System.out.printf(doC + " doC==>doF:%.4fC", doF);
         }
     }
 }
